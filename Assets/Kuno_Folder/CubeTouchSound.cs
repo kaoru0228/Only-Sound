f2eaa3ee3833f79b2735222c7456c24ee9e395e8
XPS13_9300\kunot <kunotomoki61@gmail.com>
2022-11-16 11:04:46 +0900

    場所の変更

--- Assets/Kuno_Folder/CubeTouchSound.cs
+++ Assets/Kuno_Folder/CubeTouchSound.cs
@@ -6,15 +6,15 @@ public class CubeTouchSound : MonoBehaviour
 {
     void Update()
     {
-        float dx = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
-        float dz = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
-        transform.position = new Vector3 (
-            transform.position.x + dx, 0.5f, transform.position.z + dz
-        );
+        // float dx = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+        // float dz = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        // transform.position = new Vector3 (
+        //     transform.position.x + dx, 0.5f, transform.position.z + dz
+        // );
     }
  
     void OnCollisionEnter(Collision col) {
-      if (col.gameObject.name == "Cube") {
+      if (col.gameObject.name == "Room") {
             GetComponent<AudioSource>().Play();
         }
       }
