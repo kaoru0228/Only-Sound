c0477012635720ce9f64b923e8647166bf16971c
Kaoru Ueda <z3033034@edu.gifu-u.ac.jp>
2023-01-27 16:02:41 +0900

    シーン遷移_完成版

--- Assets/GoalJudge.cs
+++ Assets/GoalJudge.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GoalJudge : MonoBehaviour{
 
     public GameObject EndTextObject;
     public GameObject Target;
     
-    private void OnTriggerEnter(Collider other){  //何かが衝突した時
+    private void OnTriggerEnter(Collider other){  //何かがすり抜けた時
         //other.gameObject.SetActive(false);  //衝突したオブジェクトを消滅
-        Target.SetActive(false);  //衝突したオブジェクト(Player)を消滅
+        //Target.SetActive(false);  //衝突したオブジェクト(Player)を消滅
         EndTextObject.SetActive(true);  //EditTextを生成
+
+        Invoke("ChangeScene", 1.5f);
     }
+    void ChangeScene(){
+        SceneManager.LoadSceneAsync("Title");  //こっちは裏でシーンの読み込みを行い、読み込み次第実行
+    }
+
     // void Start(){
         
     // }
--- Assets/Maze1_nomal.cs
+++ Assets/Maze1_nomal.cs
@@ -8,24 +8,14 @@ public class Maze1_nomal : MonoBehaviour{
     // public GameObject EndTextObject;
     // public GameObject Target;
     
-    private void OnTriggerEnter(Collider other){  //何かが衝突した時
-        //other.gameObject.SetActive(false);  //衝突したオブジェクトを消滅
-        // Target.SetActive(false);  //衝突したオブジェクト(Player)を消滅
-        // EndTextObject.SetActive(true);  //EditTextを生成
-        // ChangeScene();
+    private void OnTriggerEnter(Collider other){  //何かがすり抜けた時
         SceneManager.LoadSceneAsync("kaoru");
+        Debug.Log("aaa");
     }
-    
     void Start(){
         //Invoke("ChangeScene", 1.5f);
     }
-
     void Update(){
         
     }
-
-    // void ChangeScene(){
-    //     //SceneManager.LoadScene("Title");  //表でシーンの読み込みを行う(重くなるか塗生あり)
-    //     SceneManager.LoadSceneAsync("kaoru");  //こっちは裏でシーンの読み込みを行い、読み込み次第実行
-    // }
 }
